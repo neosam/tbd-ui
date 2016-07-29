03aed7491f69951e349e7e753b87fe8dfbf83957
Simon Goller <neosam+github@posteo.de>
2016-07-29 20:09:34 +0200

    Use json values to send data to the browser.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 #[macro_use] extern crate nickel;
-#[macro_use] extern crate json;
+//#[macro_use] extern crate json;
 extern crate rustc_serialize;
 
 use nickel::{Nickel, HttpRouter, JsonBody};
 use nickel::StaticFilesHandler;
+use rustc_serialize::json;
 
-fn get_active_tasks() -> json::JsonValue {
-    array![
-        object!{
-            "title" => "Test",
-            "deadline" => "2016-09-12"
+fn get_active_tasks() -> String {
+    let list = vec![
+        SendActiveTask {
+            title: "title1".to_string(),
+            due_date: "2016-08-20".to_string()
         },
-        object!{
-            "title" => "Test2",
-            "deadline" => "2016-10-15"
+        SendActiveTask {
+            title: "title2".to_string(),
+            due_date: "2016-09-24".to_string()
         }
-    ]
+    ];
+    json::encode(&list).unwrap()
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
@@ -26,14 +28,19 @@ struct NewActiveTask {
     due_until: i32
 }
 
+#[derive(Clone, RustcDecodable, RustcEncodable)]
+struct SendActiveTask {
+    title: String,
+    due_date: String
+}
+
 fn main() {
     let mut server = Nickel::new();
 
     server.get("/hello", middleware!("Hello World"));
     server.get("/hello2", middleware!("Hello World2"));
     server.get("/active_tasks", middleware! { | req, res |
-        let actives = get_active_tasks();
-        json::stringify_pretty(actives, 2)
+        get_active_tasks()
     });
     server.post("/add_active_task", middleware! { | req, res |
         let person = req.json_as::<NewActiveTask>().unwrap();
