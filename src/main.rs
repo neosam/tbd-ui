3657ef45abc168f19d56ce1a5b2ae364c3b719bb
Simon Goller <neosam+github@posteo.de>
2016-07-30 09:47:23 +0200

    Can create and load active tasks

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 #[macro_use] extern crate nickel;
 //#[macro_use] extern crate json;
 extern crate rustc_serialize;
+extern crate tbd;
 
 use nickel::{Nickel, HttpRouter, JsonBody};
 use nickel::StaticFilesHandler;
 use rustc_serialize::json;
+use tbd::{task, tasklog};
+use tbd::task::{ActiveTask,TaskStatTrait};
+use tbd::tasklog::TaskLog;
+use std::sync::{Arc, Mutex};
 
-fn get_active_tasks() -> String {
-    let list = vec![
-        SendActiveTask {
-            title: "title1".to_string(),
-            due_date: "2016-08-20".to_string()
-        },
-        SendActiveTask {
-            title: "title2".to_string(),
-            due_date: "2016-09-24".to_string()
-        }
-    ];
-    json::encode(&list).unwrap()
+fn get_active_tasks<T: TaskStatTrait>(task_stat: &T) -> String {
+    let active_tasks = task_stat.all_actives().unwrap_or(Vec::<ActiveTask>::new());
+    let mut res = Vec::new();
+    for a_task in active_tasks {
+        let due_date = format!("{}-{}-{}",
+                               a_task.due.tm_year + 1900,
+                               a_task.due.tm_mon + 1,
+                               a_task.due.tm_mday);
+        res.push(SendActiveTask {
+            title: a_task.task.title,
+            due_date: due_date
+        });
+    };
+    json::encode(&res).unwrap()
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
@@ -25,7 +32,7 @@ struct NewActiveTask {
     title: String,
     description: String,
     factor: f32,
-    due_until: i32
+    due_until: i16
 }
 
 #[derive(Clone, RustcDecodable, RustcEncodable)]
@@ -36,15 +43,24 @@ struct SendActiveTask {
 
 fn main() {
     let mut server = Nickel::new();
+    let mut tasklog = TaskLog::new(".tbd".to_string());
+    tasklog.load_head();
+    let tasklog_active_tasks = Arc::new(Mutex::new(tasklog));
+    let tasklog_add_active = tasklog_active_tasks.clone();
 
     server.get("/hello", middleware!("Hello World"));
     server.get("/hello2", middleware!("Hello World2"));
     server.get("/active_tasks", middleware! { | req, res |
-        get_active_tasks()
+        let tasklog = tasklog_active_tasks.lock().unwrap();
+        get_active_tasks(&*tasklog)
     });
     server.post("/add_active_task", middleware! { | req, res |
-        let person = req.json_as::<NewActiveTask>().unwrap();
-        print!("title: {}\n", person.title);
+        let new_a_task = req.json_as::<NewActiveTask>().unwrap();
+        tasklog_add_active.lock().unwrap().add_active_task(
+                                new_a_task.title,
+                                new_a_task.description,
+                                new_a_task.factor,
+                                new_a_task.due_until).unwrap();
         "true"
     });
     server.utilize(StaticFilesHandler::new("web/"));
